980661e67380eb32dbc685726a65c653cf26d830
ZipS <caominh1912@gmail.com>
2023-07-16 22:12:31 +0700

    finish backend basic

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors()); // Thêm dòng này để sử dụng middleware cors
 
-// Tạo một danh sách người dùng (đây chỉ là ví dụ, bạn cần có cơ chế lưu trữ thực tế)
 const users = [
-  { email: "user1@example.com", password: "12345678" },
-  { email: "user2@example.com", password: "12345678" }
+  { email: "user1@example.com", password: "12345678", username: "User 1" },
+  { email: "user2@example.com", password: "12345678", username: "User 2" }
 ];
 
 // Tuyến đường đăng nhập
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  // Kiểm tra xem email và mật khẩu có khớp với người dùng trong danh sách hay không
   const user = users.find(user => user.email === email && user.password === password);
 
   if (user) {
-    // Đăng nhập thành công
-    res.json({ message: "Đăng nhập thành công" });
+    const userData = {
+      email: user.email,
+      username: user.username,
+      password: password.password
+    };
+    res.json({ user: userData, message: "Đăng nhập thành công" });
   } else {
-    // Sai thông tin đăng nhập
     res.status(401).json({ error: "Email hoặc mật khẩu không chính xác" });
   }
 });
 
-// Khởi động máy chủ
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/", (req, res) => {
+  res.send("Hello this is my api");
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
 });
